9142900beecd2bd6d0c2939de727d91ff5d6da5f
Jakob <jpusterhofer@gmail.com>
2022-11-28 13:48:12 +0100

    added pure randomness

--- app.js
+++ app.js
@@ -78,26 +78,26 @@ particlesJS('particles-js', {
 // requestAnimationFrame(update)
 
 //TEXT CODE
-function type(n, t) {
-  var str = document.getElementsByTagName('code')[n].innerHTML.toString()
-  var i = 0
-  document.getElementsByTagName('code')[n].innerHTML = ''
+// function type(n, t) {
+//   var str = document.getElementsByTagName('code')[n].innerHTML.toString()
+//   var i = 0
+//   document.getElementsByTagName('code')[n].innerHTML = ''
 
-  setTimeout(function () {
-    var se = setInterval(function () {
-      i++
-      document.getElementsByTagName('code')[n].innerHTML = str.slice(0, i) + '|'
-      if (i == str.length) {
-        clearInterval(se)
-        document.getElementsByTagName('code')[n].innerHTML = str
-      }
-    }, 60)
-  }, t)
-}
+//   setTimeout(function () {
+//     var se = setInterval(function () {
+//       i++
+//       document.getElementsByTagName('code')[n].innerHTML = str.slice(0, i) + '|'
+//       if (i == str.length) {
+//         clearInterval(se)
+//         document.getElementsByTagName('code')[n].innerHTML = str
+//       }
+//     }, 60)
+//   }, t)
+// }
 
-type(0, 400)
-type(1, 4000)
-type(2, 12000)
+// type(0, 400)
+// type(1, 4000)
+// type(2, 12000)
 
 // RELLAX
 
